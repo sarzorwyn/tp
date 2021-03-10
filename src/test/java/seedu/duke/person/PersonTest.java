c22a92e14ef6c224a6ae5ffc94f71eef5cceea6a
hussain1998 <e0406406@u.nus.edu>
2021-03-10 10:56:57 +0800

    Add testutil package. Consists of PersonBuilder and SamplePersons class.

--- src/test/java/seedu/duke/person/PersonTest.java
+++ src/test/java/seedu/duke/person/PersonTest.java
@@ -4,31 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.duke.testutil.SamplePersons.JOHN;
+import static seedu.duke.testutil.SamplePersons.JOHN_DIFF_ID;
+import static seedu.duke.testutil.SamplePersons.JOHN_NO_PHONE;
 
 class PersonTest {
 
     @Test
     void testEquals() {
-        Person p1 = constructPerson("123A", "John", "12345678");
-        Person p2 = constructPerson("123A", "John", null);
-        Person p3 = constructPerson("456B", "John", "12345678");
-
-        assertTrue(p1.equals(p1));
-        assertTrue(p1.equals(p2));
-        assertFalse(p1.equals(p3));
-        assertFalse(p1.equals("test string"));
-    }
-
-    Person constructPerson(String idString, String nameString, String phoneNo) {
-        Id personId = new Id(idString);
-        Name personName = new Name(nameString);
-        Phone personPhone;
-        if (phoneNo == null) {
-            personPhone = new Phone();
-        } else {
-            personPhone = new Phone(phoneNo);
-        }
-        Person person = new Person(personId, personName, personPhone);
-        return person;
+        assertTrue(JOHN.equals(JOHN));
+        assertTrue(JOHN.equals(JOHN_NO_PHONE));
+        assertFalse(JOHN.equals(JOHN_DIFF_ID));
+        assertFalse(JOHN.equals("test string"));
     }
 }
