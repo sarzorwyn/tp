d4fc1ba6e4b97041b0a04bbca9dfcdbf7eeaca88
iamakilahamed <akilahamed123@gmail.com>
2021-03-14 18:59:58 +0800

    Add assertion in CheckoutCommand class
    Not working

--- src/main/java/seedu/duke/commands/CheckoutCommand.java
+++ src/main/java/seedu/duke/commands/CheckoutCommand.java
@@ -3,6 +3,7 @@ package seedu.duke.commands;
 
 import seedu.duke.exceptions.PersonNotFoundException;
 import seedu.duke.person.Id;
+import seedu.duke.person.Name;
 import seedu.duke.person.Person;
 import seedu.duke.person.TrackingList;
 
@@ -15,12 +16,13 @@ public class CheckoutCommand extends Command {
     public static final String COMMAND = "checkout";
     public static final String CHECKOUT_MESSAGE = "%s has been successfully checked-out!";
 
-    private Id id;
+    private final Id id;
+    private final Name name;
     private Person toCheckout;
 
-    public CheckoutCommand(String idString,String name) {
+    public CheckoutCommand(String idString,String nameString) {
         id = new Id(idString);
-
+        name = new Name(nameString);
     }
 
     public Person getToCheckout() {
@@ -34,11 +36,11 @@ public class CheckoutCommand extends Command {
     @Override
     public CommandOutput execute(TrackingList trackingList) throws PersonNotFoundException {
         toCheckout = trackingList.findExactPerson(id);
+        assert toCheckout.getName() == name : "name does not match id";
         if (toCheckout == null) {
             throw new PersonNotFoundException();
-        } else {
-            toCheckout.setCheckedIn(false);
         }
+        toCheckout.setCheckedIn(false);
         return new CommandOutput(String.format(CHECKOUT_MESSAGE, toCheckout.getName()), COMMAND);
     }
 
