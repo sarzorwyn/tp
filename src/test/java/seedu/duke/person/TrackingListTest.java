dee134747cb545a98df1b09863673ddac5ee2eb6
Ivan <ivan.t.mailbox@gmail.com>
2021-03-16 01:35:58 +0800

    Added more unit tests to trackingList

--- src/test/java/seedu/duke/person/TrackingListTest.java
+++ src/test/java/seedu/duke/person/TrackingListTest.java
@@ -1,7 +1,12 @@
 package seedu.duke.person;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.duke.testutil.SamplePersons.JOHN;
@@ -12,12 +17,97 @@ import static seedu.duke.testutil.SamplePersons.MARY;
 public class TrackingListTest {
     private TrackingList trackingList;
 
+    @BeforeEach
+    public void initTrackingList() {
+        trackingList = new TrackingList();
+    }
+
     @Test
-    void testConstructor() {
-        trackingList = new TrackingList(JOHN, MARY, ALICE);
-        assertTrue(trackingList.contains(JOHN));
-        assertTrue(trackingList.contains(MARY));
+    public void testConstructor() {
+        TrackingList constructedList = new TrackingList(JOHN, MARY, ALICE);
+        assertTrue(constructedList.contains(JOHN));
+        assertTrue(constructedList.contains(MARY));
+        assertTrue(constructedList.contains(ALICE));
+        assertFalse(constructedList.contains(BOB));
+    }
+
+    @Test
+    public void testContains() {
+        trackingList.add(ALICE);
+        trackingList.add(BOB);
         assertTrue(trackingList.contains(ALICE));
+        assertFalse(trackingList.contains(JOHN));
         assertFalse(trackingList.contains(BOB));
+    }
+
+    @Test
+    public void testDelete() {
+        trackingList.add(ALICE);
+        trackingList.add(BOB);
+        trackingList.add(MARY);
+
+        trackingList.delete(ALICE);
+        int expectedBobIndex = 0;
+        trackingList.delete(expectedBobIndex);
+
+        assertFalse(trackingList.contains(ALICE));
+        assertFalse(trackingList.contains(BOB));
+        assertTrue(trackingList.contains(MARY));
+    }
+
+    @Test
+    public void testGetPerson() {
+        trackingList.add(JOHN);
+        trackingList.add(MARY);
+        int expectedJohnIndex = 0;
+
+        assertEquals(JOHN,trackingList.getPerson(expectedJohnIndex));
+    }
+
+    @Test
+    public void testListPerson() {
+        trackingList.add(ALICE);
+        trackingList.add(BOB);
+        trackingList.add(MARY);
+
+        List<Person> returnedList = trackingList.listPerson();
+
+        assertTrue(returnedList.contains(ALICE));
+        assertTrue(returnedList.contains(BOB));
+        assertTrue(returnedList.contains(MARY));
+    }
+
+    @Test
+    public void testGetSize() {
+        int expectedSize = 0;
+        assertEquals(trackingList.getSize(), expectedSize);
+
+        trackingList.add(ALICE);
+        expectedSize += 1;
+        assertEquals(trackingList.getSize(), expectedSize);
+
+        trackingList.add(BOB);
+        expectedSize += 1;
+        assertEquals(trackingList.getSize(), expectedSize);
+    }
+
+    @Test
+    public void testClear() {
+        trackingList.add(ALICE);
+        trackingList.add(BOB);
+        trackingList.add(MARY);
+
+        trackingList.clear();
+        assertFalse(trackingList.contains(ALICE));
+        assertFalse(trackingList.contains(BOB));
+        assertFalse(trackingList.contains(MARY));
+
+        int expectedSize = 0;
+        assertEquals(trackingList.getSize(), expectedSize);
+    }
+
+    @AfterEach
+    public void clearTrackingList() {
+        trackingList.clear();
     }
 }
