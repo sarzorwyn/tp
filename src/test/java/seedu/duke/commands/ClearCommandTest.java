bcbea2b8865ea2e326f5b6e774982ce0ecce8cfe
iamakilahamed <akilahamed123@gmail.com>
2021-03-21 20:36:17 +0800

    Use a private TrackingList for ClearCommandTest instead of using the sample tracking list

--- src/test/java/seedu/duke/commands/ClearCommandTest.java
+++ src/test/java/seedu/duke/commands/ClearCommandTest.java
@@ -1,18 +1,22 @@
 package seedu.duke.commands;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.person.TrackingList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static seedu.duke.testutil.SampleTrackingList.SAMPLE_TRACKING_LIST;
+import static seedu.duke.testutil.SamplePersons.ALICE;
+import static seedu.duke.testutil.SamplePersons.JOHN;
+import static seedu.duke.testutil.SamplePersons.MARY;
 
 public class ClearCommandTest {
+
+    private final TrackingList trackingList = new TrackingList(ALICE, JOHN, MARY);
 
     @Test
     public void testClear() {
         ClearCommand clear = new ClearCommand();
-        clear.execute(SAMPLE_TRACKING_LIST);
-        assertEquals(0, SAMPLE_TRACKING_LIST.getSize());
+        clear.execute(trackingList);
+        assertEquals(0, trackingList.getSize());
     }
 
 }
