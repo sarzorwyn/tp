db3248dda8a314c98625618106a687e1ffac2ee9
iamakilahamed <akilahamed123@gmail.com>
2021-03-21 21:12:53 +0800

    Shift the filtering of checkedin people logic into ListCheckedInCommand

--- src/main/java/seedu/duke/commands/ListCheckedInCommand.java
+++ src/main/java/seedu/duke/commands/ListCheckedInCommand.java
@@ -5,9 +5,16 @@ import seedu.duke.person.TrackingList;
 public class ListCheckedInCommand extends Command {
 
     public static final String COMMAND = "list";
+    private final TrackingList checkedInList = new TrackingList();
 
     @Override
     public CommandOutput execute(TrackingList trackingList) {
-        return new CommandOutput(trackingList.listPerson(), COMMAND);
+        for (int i = 0; i < trackingList.getSize(); i++) {
+            if (trackingList.getPerson(i).getCheckedIn()) {
+                checkedInList.add(trackingList.getPerson(i));
+            }
+        }
+        return new CommandOutput(checkedInList.listPerson(), COMMAND);
     }
+
 }
