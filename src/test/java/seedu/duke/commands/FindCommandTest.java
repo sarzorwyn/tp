a92db6feb81760095f94319d750712e148b6ec45
iamakilahamed <akilahamed123@gmail.com>
2021-03-21 20:36:48 +0800

    Complete FindCommand test

--- src/test/java/seedu/duke/commands/FindCommandTest.java
+++ src/test/java/seedu/duke/commands/FindCommandTest.java
@@ -1,2 +1,29 @@
-package seedu.duke.commands;public class FindCommandTest {
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.common.Messages;
+import seedu.duke.exceptions.PersonNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static seedu.duke.testutil.SampleTrackingList.SAMPLE_TRACKING_LIST;
+
+public class FindCommandTest {
+
+    @Test
+    public void testFind() throws PersonNotFoundException {
+        FindCommand find = new FindCommand("665B");
+        String foundPersonName = find.execute(SAMPLE_TRACKING_LIST).person.getName().nameString;
+        assertEquals("Alice", foundPersonName);
+    }
+
+    @Test
+    public void testFindPersonNotFound() {
+        FindCommand find = new FindCommand("230C");
+        Throwable exception = assertThrows(PersonNotFoundException.class, () ->
+                find.execute(SAMPLE_TRACKING_LIST));
+        assertEquals(Messages.PERSON_NOT_FOUND, exception.getMessage());
+    }
+
 }
