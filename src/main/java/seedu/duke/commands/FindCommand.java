3269c35db8fbb299cc189bf0e0d65b8b01a31920
iamakilahamed <akilahamed123@gmail.com>
2021-03-22 19:05:16 +0800

    Update FindCommand to return the checked-in status

--- src/main/java/seedu/duke/commands/FindCommand.java
+++ src/main/java/seedu/duke/commands/FindCommand.java
@@ -7,11 +7,13 @@ import seedu.duke.person.TrackingList;
 
 /**
  * Finds the person in tracking list using id.
+ * Returns if the person has checked-in or not.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND = "find";
-    public static final String FIND_MESSAGE = "%s found.";
+    public static final String CHECKED_IN_MESSAGE = "%s found is checked-in.";
+    public static final String NOT_CHECKED_IN_MESSAGE = "%s found has not checked-in.";
     private final Id idKeyword;
 
     public FindCommand(String idKeyword) {
@@ -21,7 +23,11 @@ public class FindCommand extends Command {
     @Override
     public CommandOutput execute(TrackingList trackingList) throws PersonNotFoundException {
         Person person = trackingList.findExactPerson(idKeyword);
-        return new CommandOutput(person, String.format(FIND_MESSAGE,person.getName()), COMMAND);
+        boolean isCheckedIn = person.getCheckedIn();
+        if (!isCheckedIn) {
+            return new CommandOutput(person, String.format(NOT_CHECKED_IN_MESSAGE,person.getName()), COMMAND);
+        }
+        return new CommandOutput(person, String.format(CHECKED_IN_MESSAGE,person.getName()), COMMAND);
     }
 
 }
