e7e25fffb81a5e865268fa1e9195dc5c49f199bb
iamakilahamed <akilahamed123@gmail.com>
2021-03-23 11:35:27 +0800

    Add EditMax Command

--- src/main/java/seedu/duke/commands/EditMaxCommand.java
+++ src/main/java/seedu/duke/commands/EditMaxCommand.java
@@ -1,2 +1,26 @@
-package seedu.duke.commands;public class EditMaxCommand {
+package seedu.duke.commands;
+
+import seedu.duke.Duke;
+import seedu.duke.person.TrackingList;
+
+/**
+ * Set a new maximum capacity of the location.
+ */
+public class EditMaxCommand extends Command {
+
+    public static final String COMMAND = "editmax";
+    public static final String EDIT_MAX_MESSAGE = "Max capacity: %d";
+
+    private final int newMaxCapacity;
+
+    public EditMaxCommand(int newMaxCapacity) {
+        this.newMaxCapacity = newMaxCapacity;
+    }
+
+    @Override
+    public CommandOutput execute(TrackingList trackingList) {
+        Duke.location.setMaxCapacity(newMaxCapacity);
+        return new CommandOutput(String.format(EDIT_MAX_MESSAGE, newMaxCapacity), COMMAND);
+    }
+
 }
