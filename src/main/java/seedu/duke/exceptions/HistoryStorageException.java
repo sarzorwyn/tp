a510fd4e472b828f664d4ddaf73101f4c96d151c
Lim Wen Feng <60415251+limwenfeng@users.noreply.github.com>
2021-03-28 00:28:45 +0800

    Update HistoryStorageException.java

--- src/main/java/seedu/duke/exceptions/HistoryStorageException.java
+++ src/main/java/seedu/duke/exceptions/HistoryStorageException.java
@@ -2,7 +2,7 @@ package seedu.duke.exceptions;
 
 /**
  * Indicates that there has been an error when converting data or read/write between the application
- * and the storage file.
+ * and the history file.
  */
 public class HistoryStorageException extends Exception {
 
