2b04e4224c2536a6d26554f3c6672b49a483110e
iamakilahamed <akilahamed123@gmail.com>
2021-03-31 19:30:34 +0800

    Add java doc

--- src/main/java/seedu/duke/commands/EditMaxCommand.java
+++ src/main/java/seedu/duke/commands/EditMaxCommand.java
@@ -1,7 +1,6 @@
 package seedu.duke.commands;
 
 import seedu.duke.Duke;
-import seedu.duke.common.Messages;
 import seedu.duke.exceptions.InvalidMaxCapacityException;
 import seedu.duke.location.Location;
 import seedu.duke.person.TrackingList;
@@ -9,29 +8,46 @@ import seedu.duke.person.TrackingList;
 import static seedu.duke.common.Messages.INVALID_MAX_CAPACITY;
 
 /**
- * Set a new maximum capacity of the location.
+ * Set a new maximum capacity for the venue.
  */
 public class EditMaxCommand extends Command {
 
     public static final String COMMAND = "editmax";
     private static final String NEW_CAPACITY_REGEX = "^[0-9]{1,6}$";
     public static final String EDIT_MAX_MESSAGE = "New max capacity: %d";
-
     private final int newMaxCapacity;
     private final Location location;
 
+    /**
+     * Creates a EditMax Command to edit the maximum capacity of the venue.
+     *
+     * @param newMaxCapacity new capacity limit provided by user
+     * @throws InvalidMaxCapacityException if the new capacity provided does not match the requirements
+     */
     public EditMaxCommand(String newMaxCapacity) throws InvalidMaxCapacityException {
         checkCapacityValidity(newMaxCapacity);
         this.newMaxCapacity = Integer.parseInt(newMaxCapacity);
         location = Duke.getInstance().getLocation();
     }
 
+    /**
+     * Checks if the new maximum capacity matches the requirements.
+     *
+     * @param newMaxCapacity new capacity limit provided by user
+     * @throws InvalidMaxCapacityException if the new capacity provided does not match the requirements
+     */
     private void checkCapacityValidity(String newMaxCapacity) throws InvalidMaxCapacityException {
         if (!newMaxCapacity.matches(NEW_CAPACITY_REGEX) || Integer.parseInt(newMaxCapacity) < 0) {
             throw new InvalidMaxCapacityException(INVALID_MAX_CAPACITY);
         }
     }
 
+    /**
+     * Executes the EditMaxCommand.
+     *
+     * @param trackingList list of visitors
+     * @return a success message to user
+     */
     @Override
     public CommandOutput execute(TrackingList trackingList) {
         location.setMaxCapacity(newMaxCapacity);
