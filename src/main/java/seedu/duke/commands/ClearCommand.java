dbf2c2f29ff8e044813b5831d7028b25498ed214
iamakilahamed <akilahamed123@gmail.com>
2021-03-31 19:12:42 +0800

    Add java doc

--- src/main/java/seedu/duke/commands/ClearCommand.java
+++ src/main/java/seedu/duke/commands/ClearCommand.java
@@ -3,13 +3,19 @@ package seedu.duke.commands;
 import seedu.duke.person.TrackingList;
 
 /**
- * Clears all the entries in tracking list.
+ * Clears all the entries in trackingList.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND = "clear";
     public static final String CLEAR_MESSAGE = "Cleared %d entries";
 
+    /**
+     * Executes the ClearCommand.
+     *
+     * @param trackingList the list of visitors
+     * @return a message to user indicating the number of entries cleared
+     */
     @Override
     public CommandOutput execute(TrackingList trackingList) {
         int entriesCleared = trackingList.getSize();
