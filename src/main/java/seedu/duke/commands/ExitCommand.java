54be92def01d990fc2aadf2fa60244b7d41d0de0
iamakilahamed <akilahamed123@gmail.com>
2021-03-31 19:38:55 +0800

    Add java doc

--- src/main/java/seedu/duke/commands/ExitCommand.java
+++ src/main/java/seedu/duke/commands/ExitCommand.java
@@ -10,6 +10,12 @@ public class ExitCommand extends Command {
     public static final String COMMAND = "exit";
     public static final String EXIT_MESSAGE = "Exiting Control Your Crowd...";
 
+    /**
+     * Executes the ExitCommand.
+     *
+     * @param trackingList list of visitors
+     * @return displays the exit message to the user
+     */
     @Override
     public CommandOutput execute(TrackingList trackingList) {
         return new CommandOutput(EXIT_MESSAGE, COMMAND);
