19a91205e873698f30fd3ea65afb825d5214a76b
JonathanKhooTY <jonathan.khooty@gmail.com>
2021-04-04 20:29:28 +0800

    Update find bug

--- src/main/java/seedu/duke/parser/FindParser.java
+++ src/main/java/seedu/duke/parser/FindParser.java
@@ -3,7 +3,9 @@ package seedu.duke.parser;
 import seedu.duke.commands.Command;
 import seedu.duke.commands.FindCommand;
 import seedu.duke.common.Messages;
+import seedu.duke.exceptions.InvalidIdException;
 import seedu.duke.exceptions.WrongFlagException;
+import seedu.duke.person.Id;
 
 /**
  * Finds a user and returns the user's details if user's ID is input.
@@ -16,12 +18,17 @@ public class FindParser extends Parser {
      * @return Returns FindCommand
      * @throws WrongFlagException if ID flag is not utilised.
      */
-    protected static Command parseFind(String argument) throws WrongFlagException {
+    protected static Command parseFind(String argument) throws WrongFlagException, InvalidIdException {
         String id;
         if (argument.startsWith("i/")) {
             id = argument.substring(2);
         } else {
             throw new WrongFlagException(Messages.WRONG_FLAG);
+        }
+
+
+        if (!Id.isValidId(id)) {
+            throw new InvalidIdException(Messages.ID_ERROR);
         }
         return new FindCommand(id);
     }
