849237d9729918dc716acc6c9e5e2551bf8f4370
JonathanKhooTY <jonathan.khooty@gmail.com>
2021-04-05 21:17:10 +0800

    Reverted #210

--- src/main/java/seedu/duke/parser/CheckInParser.java
+++ src/main/java/seedu/duke/parser/CheckInParser.java
@@ -87,10 +87,7 @@ public class CheckInParser extends Parser {
         if (!Id.isValidId(id)) {
             throw new InvalidIdException(Messages.ID_ERROR);
         }
-
-        if (flagChecker(name) != -1) {
-            throw new WrongFlagException(Messages.WRONG_FLAG);
-        }
+        
 
         if (!Name.isValidName(name)) {
             throw new InvalidNameFormatException(Messages.NAME_ERROR);
