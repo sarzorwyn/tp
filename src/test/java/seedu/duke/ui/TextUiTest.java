587282f632e0b4bf1dd64c25f39cdd5fdcacd444
limwenfeng <wilfred.limwf@gmail.com>
2021-04-09 23:00:36 +0800

    added test

--- src/test/java/seedu/duke/ui/TextUiTest.java
+++ src/test/java/seedu/duke/ui/TextUiTest.java
@@ -1,1 +1,41 @@
+package seedu.duke.ui;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.PersonNotFoundException;
+import seedu.duke.model.TrackingList;
+import seedu.duke.model.person.Person;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.duke.testutil.SamplePersons.JOHN;
+import static seedu.duke.testutil.SamplePersons.ALICE;
+import static seedu.duke.testutil.SamplePersons.BOB;
+import static seedu.duke.testutil.SamplePersons.MARY;
+
+public class TextUiTest {
+    TextUi textUi = new TextUi();
+    private TrackingList trackingList;
+
+    @BeforeEach
+    public void initTrackingList() {
+        trackingList = new TrackingList();
+        trackingList.add(ALICE);
+    }
+
+    @Test
+    public void testConstructor() {
+        int actualCurrentCapacity = textUi.getCurrentCapacity(trackingList.listPerson());
+        assertEquals(1, actualCurrentCapacity);
+    }
+
+    @AfterEach
+    public void clearTrackingList() {
+        trackingList.clear();
+    }
+}
