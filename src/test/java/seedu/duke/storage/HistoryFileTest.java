9dbba1de59324e14fab7e07bf7ed7199925ffc3e
limwenfeng <wilfred.limwf@gmail.com>
2021-04-10 00:32:45 +0800

    added history test file

--- src/test/java/seedu/duke/storage/HistoryFileTest.java
+++ src/test/java/seedu/duke/storage/HistoryFileTest.java
@@ -1,24 +1,16 @@
 package seedu.duke.storage;
 
 import org.junit.jupiter.api.Test;
-import seedu.duke.exceptions.HistoryStorageException;
-import seedu.duke.datetime.DateTime;
-import seedu.duke.exceptions.InvalidArgumentSizeException;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static seedu.duke.testutil.SamplePersons.ALICE;
 
 public class HistoryFileTest {
     private static final String TEST_DATA_PATH = "src/test/data/HistoryFileTest/";
     private static final String VALID_DATA_FILE = "TestHistory";
-    private static final String NEW_FILE = "NewFile";
+    private static final String NEW_FILE = "NewHistoryFile";
 
     @Test
     public void invalidFilePath_HistoryStorageExceptionThrown() {
