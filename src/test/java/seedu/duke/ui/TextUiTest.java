1e7994e5b1bc27f9f27c75e4f1315b378a83f01f
limwenfeng <wilfred.limwf@gmail.com>
2021-04-09 22:10:04 +0800

    use sample tracking list

--- src/test/java/seedu/duke/ui/TextUiTest.java
+++ src/test/java/seedu/duke/ui/TextUiTest.java
@@ -13,27 +13,16 @@ import static seedu.duke.testutil.SamplePersons.ALICE;
 import static seedu.duke.testutil.SamplePersons.BOB;
 import static seedu.duke.testutil.SamplePersons.MARY;
 import static seedu.duke.testutil.SamplePersons.JOHN;
+import static seedu.duke.testutil.SampleTrackingList.SAMPLE_TRACKING_LIST;
 
 public class TextUiTest {
     private TrackingList trackingList;
     TextUi textUi = new TextUi();
 
-
-    @BeforeEach
-    public void initTrackingList() {
-        trackingList = new TrackingList();
-    }
-
     @Test
     public void testGetCurrentCapacity() {
-        TrackingList trackingList = new TrackingList(JOHN, MARY, ALICE);
         int expectedCurrentCapacity = 3;
-        int actualCurrentCapacity = textUi.getCurrentCapacity(trackingList.listPerson());
+        int actualCurrentCapacity = textUi.getCurrentCapacity(SAMPLE_TRACKING_LIST.listPerson());
         assertEquals(expectedCurrentCapacity, actualCurrentCapacity);
-    }
-
-    @AfterEach
-    public void clearTrackingList() {
-        trackingList.clear();
     }
 }
